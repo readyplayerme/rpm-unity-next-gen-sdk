b89ca8d546fed95c713cb635321e6c3a3b52f5b3
Sercan Altundas <srcnalt@users.noreply.github.com>
2024-05-02 14:07:47 +0300

    [chore] test fixes

--- Tests/Api/V1/AvatarApiTests.cs
+++ Tests/Api/V1/AvatarApiTests.cs
@@ -1,32 +1,32 @@
 using System;
 using UnityEngine;
 using NUnit.Framework;
-using UnityEngine.TestTools;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using ReadyPlayerMe.Runtime.Api.V1.Characters;
-using ReadyPlayerMe.Runtime.Api.V1.Characters.Models;
+using ReadyPlayerMe.Runtime.Api.V1.Avatars;
+using ReadyPlayerMe.Runtime.Api.V1.Avatars.Models;
+using UnityEngine.TestTools;
 
 namespace ReadyPlayerMe.Tests.Api.V1
 {
-    public class CharacterApiTests
+    public class AvatarApiTests
     {
-        private readonly CharacterApi characterApi = new CharacterApi();
+        private readonly AvatarApi characterApi = new AvatarApi();
         
         [Test, Order(0)]
         public async Task Create_Character()
         {
             // Arrange
-            var request = new CharacterCreateRequest()
+            var request = new AvatarCreateRequest()
             {
-                Payload = new CharacterCreateRequestBody
+                Payload = new AvatarCreateRequestBody
                 {
                     ApplicationId = "6628c280ecb07cb9d9cd7238"
                 }
             };
             
             // Act
-            var response = await characterApi.CreateCharacterAsync(request);
+            var response = await characterApi.CreateAvatarAsync(request);
             Debug.Log(response.Data.GlbUrl);
             
             // Assert
@@ -37,10 +37,10 @@ namespace ReadyPlayerMe.Tests.Api.V1
         public async Task Update_Character()
         {
             // Arrange
-            var request = new CharacterUpdateRequest()
+            var request = new AvatarUpdateRequest()
             {
-                CharacterId = "6628d1c497cb7a2453b807b1",
-                Payload = new CharacterUpdateRequestBody()
+                AvatarId = "6628d1c497cb7a2453b807b1",
+                Payload = new AvatarUpdateRequestBody()
                 {
                     Assets = new Dictionary<string, string>
                     {
@@ -50,7 +50,7 @@ namespace ReadyPlayerMe.Tests.Api.V1
             };
             
             // Act
-            var response = await characterApi.UpdateCharacterAsync(request);
+            var response = await characterApi.UpdateAvatarAsync(request);
             Debug.Log(response.Data.GlbUrl);
             
             // Assert
@@ -61,10 +61,10 @@ namespace ReadyPlayerMe.Tests.Api.V1
         public async Task Preview_Character()
         {
             // Arrange
-            var request = new CharacterPreviewRequest()
+            var request = new AvatarPreviewRequest()
             {
-                CharacterId = "6628d1c497cb7a2453b807b1",
-                Params = new CharacterPreviewQueryParams()
+                AvatarId = "6628d1c497cb7a2453b807b1",
+                Params = new AvatarPreviewQueryParams()
                 {
                     Assets = new Dictionary<string, string>
                     {
@@ -74,7 +74,7 @@ namespace ReadyPlayerMe.Tests.Api.V1
             };
             
             // Act
-            var response = await characterApi.PreviewCharacterAsync(request);
+            var response = await characterApi.PreviewAvatarAsync(request);
             
             // Assert
             Assert.NotNull(response);
