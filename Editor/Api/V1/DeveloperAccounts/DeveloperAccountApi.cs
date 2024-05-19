6fb45249bb4abd02409a448506f54ec5da6196ce
MaxAndreassenRPM <max@wolf3d.io>
2024-05-19 08:46:39 +0100

    fix: prevents warnings of editor-side SDK code from the dev

--- Editor/Api/V1/DeveloperAccounts/DeveloperAccountApi.cs
+++ Editor/Api/V1/DeveloperAccounts/DeveloperAccountApi.cs
@@ -10,6 +10,7 @@ namespace ReadyPlayerMe.Editor.Api.V1.DeveloperAccounts
     {
         public DeveloperAccountApi()
         {
+            LogWarnings = false;
             SetAuthenticationStrategy(new DeveloperTokenAuthStrategy());
         }
 
