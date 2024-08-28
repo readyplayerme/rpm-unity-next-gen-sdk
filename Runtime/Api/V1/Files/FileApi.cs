76439c5efb5f72a4639eae376a8e8a825779bac8
Sercan Altundas <srcnalt@users.noreply.github.com>
2024-08-28 14:40:02 +0300

    Update FileApi.cs

--- Runtime/Api/V1/Files/FileApi.cs
+++ Runtime/Api/V1/Files/FileApi.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -8,6 +9,7 @@ namespace ReadyPlayerMe.Api.V1
 {
     public class FileApi
     {
+        
         public virtual async Task<Texture2D> DownloadImageAsync(string url)
         {
             var request = UnityWebRequestTexture.GetTexture(url);
@@ -19,6 +21,30 @@ namespace ReadyPlayerMe.Api.V1
             }
         
             return DownloadHandlerTexture.GetContent(request);
+        }
+        
+        // TODO: Move the string path to a class of constants
+        public virtual async Task<Texture2D> DownloadAssetIconAsync(Asset asset)
+        {
+            string iconsFolderPath = Application.persistentDataPath + "/Local Cache/Assets/Icons";
+            if(!File.Exists(iconsFolderPath + "/" + asset.Id))
+            {
+                // download thumbnail
+                using UnityWebRequest iconRequest = UnityWebRequest.Get(asset.IconUrl);
+                iconRequest.downloadHandler = new DownloadHandlerFile(iconsFolderPath + "/" + asset.Id);
+                AsyncOperation iconOp = iconRequest.SendWebRequest();
+                while (!iconOp.isDone) await Task.Yield();
+            }
+            
+            return await GetTextureFromFile(iconsFolderPath + "/" + asset.Id);
+        }
+        
+        private async Task<Texture2D> GetTextureFromFile(string path)
+        {
+            byte[] bytes = await File.ReadAllBytesAsync(path);
+            var texture = new Texture2D(2, 2);
+            texture.LoadImage(bytes);
+            return texture;
         }
 
         public virtual async Task<Texture2D[]> DownloadImagesAsync(IEnumerable<string> urls)
