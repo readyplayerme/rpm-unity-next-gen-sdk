207a00c4850344198db3cdda5e05d5e05943e81f
Harrison <harrisonhough4@gmail.com>
2024-10-14 15:42:14 +0300

    feat: added user login and user create functions

---
+++ Runtime/Api/V1/Auth/Models/CreateUserResponse.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace ReadyPlayerMe.Api.V1
+{
+    public class CreateUserResponse : ApiResponse
+    {
+        [JsonProperty("data")]
+        public CreateUserResponseBody Data { get; set; } = new CreateUserResponseBody();
+    }
+    
+    public class CreateUserResponseBody
+    {
+        [JsonProperty("id")]
+        public string Id;
+        [JsonProperty("name")]
+        public string Name;
+        [JsonProperty("email")]
+        public string Email;
+        [JsonProperty("token")]
+        public string Token;
+    }
+}
---
+++ Runtime/Api/V1/Auth/Models/LoginWithCodeResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace ReadyPlayerMe.Api.V1
+{
+    public class LoginWithCodeResponse : ApiResponse
+    {
+        [JsonProperty("email")]
+        public string Email;
+        
+        [JsonProperty("name")]
+        public string Name;
+        
+        [JsonProperty("token")]
+        public string Token;
+        
+        [JsonProperty("refreshToken")]
+        public string RefreshToken;
+    }
+}
