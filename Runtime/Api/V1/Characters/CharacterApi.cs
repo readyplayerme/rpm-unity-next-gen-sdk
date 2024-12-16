3aa4f799db0fe5a3e0ae9f15daa899720609527f
Harrison <harrisonhough4@gmail.com>
2024-12-16 12:09:59 +0200

    feat: cleaning first pass

--- Runtime/Api/V1/Characters/CharacterApi.cs
+++ Runtime/Api/V1/Characters/CharacterApi.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using UnityEngine;
-using UnityEngine.Networking;
 
 namespace ReadyPlayerMe.Api.V1
 {
@@ -14,38 +11,6 @@ namespace ReadyPlayerMe.Api.V1
         public CharacterApi()
         {
             SetAuthenticationStrategy(new ApiKeyAuthStrategy());
-        }
-
-        public virtual async Task<CharacterCreateResponse> CreateAsync(CharacterCreateRequest request, CancellationToken cancellationToken = default)
-        {
-            return await Dispatch<CharacterCreateResponse, CharacterCreateRequestBody>(
-                new ApiRequest<CharacterCreateRequestBody>
-                {
-                    Url = $"{Settings.ApiBaseUrl}/v1/{Resource}",
-                    Method = UnityWebRequest.kHttpVerbPOST,
-                    Payload = request.Payload,
-                    Headers = new Dictionary<string, string>()
-                    {
-                        { "Content-Type", "application/json" },
-                    }
-                }, cancellationToken
-            );
-        }
-
-        public virtual async Task<CharacterUpdateResponse> UpdateAsync(CharacterUpdateRequest request, CancellationToken cancellationToken = default)
-        {
-            return await Dispatch<CharacterUpdateResponse, CharacterUpdateRequestBody>(
-                new ApiRequest<CharacterUpdateRequestBody>()
-                {
-                    Url = $"{Settings.ApiBaseUrl}/v1/{Resource}/{request.Id}",
-                    Method = "PATCH",
-                    Payload = request.Payload,
-                    Headers = new Dictionary<string, string>()
-                    {
-                        { "Content-Type", "application/json" },
-                    }
-                }, cancellationToken
-            );
         }
 
         public virtual async Task<CharacterFindByIdResponse> FindByIdAsync(CharacterFindByIdRequest request, CancellationToken cancellationToken = default)
--- Tests/TestConstants.cs
+++ Tests/TestConstants.cs
@@ -3,7 +3,6 @@ namespace ReadyPlayerMe.Tests
     public class TestConstants
     {
         public const string ApplicationId = "665e05a50c62c921e5a6ab84";
-        public const string TopAssetId = "6662d230aa5829cec953c850";
         public const string CharacterId = "667182f34806feb40dc3a41c";
         public const string TemplateId = "665e05e758e847063761c985";
     }
