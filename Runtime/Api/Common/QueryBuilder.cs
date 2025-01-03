b65ae1373239517fe55d4f891e1f2ffd350bce41
Harrison <harrisonhough4@gmail.com>
2025-01-03 07:29:08 +0200

    chore: fix incorrect namespace

--- Runtime/Api/Common/QueryBuilder.cs
+++ Runtime/Api/Common/QueryBuilder.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using Unity.Plastic.Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace ReadyPlayerMe
 {
